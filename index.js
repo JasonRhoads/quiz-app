73ad1819725a3b94d03c6c52d17a552f1fd02255
Jason Rhoads <jasrhoads@gmail.com>
2023-07-13 11:28:41 -0700

    add quiz results section

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ const answerList = {
     },
 };
 
+let answers = ['shopCommerce','devNecAll','shopifyDevsAll'];
+
 
 function setQuestionNumbers() {
     document.getElementById("qNum").innerText = currentQuestion;
@@ -78,13 +80,45 @@ function submit() {
         saveAnswer(currentQuestion);
         document.getElementById('question3').classList.remove('active-question');
         document.getElementsByTagName('h2')[0].style.display = "none";
+        showScorePage();
         console.log(JSON.stringify(answerList));
+        console.log(correctAnswers);
     } else {
         window.alert('please select an answer');
     }   
 }
 
 
-function showScore() {
+function showScorePage() {
+    checkScore();
+    document.getElementById('result').style.display = "block";
+    document.getElementById('scorePercent').innerText = (correctAnswers / qTotal * 100).toFixed(0);
+    document.getElementById('answerMessage').innerText = (getScoreMessage());
+}
 
+function checkScore() {
+    for (let i = 1; i <= qTotal; i++) {
+        if(answerList[i].answer === answers[i - 1]) {
+            correctAnswers++;
+        }
+    }
+}
+
+function getScoreMessage() {
+    let msg = "";
+    switch(correctAnswers){
+        case 0:
+            msg = "Dang! You did not get any right.";
+            break;
+        case 1:
+            msg = "Nice Try! You got 1 right";
+            break;
+        case 2:
+            msg = `2 out of ${qTotal} aint bad!`;
+            break;
+        case 3:
+            msg = "You got them ALL right! Great Job!";
+            break;    
+    }
+    return msg;
 }
